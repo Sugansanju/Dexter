f2cec891edc26acacf7a46bde67b45a525bf43e0
unknown <suganjo15@gmail.com>
2018-05-21 19:02:50 +0530

    two navbars

---
+++ src/components/individual.vue
@@ -0,0 +1,425 @@
+<template>
+  <div class="hello">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <a class="navbar-brand" href="#">
+         <img src="../assets/dexter.png" width="50" height="50" alt="Dexter">Dexter Dojo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <form class="form-inline">
+         <div class="inset">
+           <img src="../assets/profile.png">
+         </div>
+         <div class="dropdown px-3">
+     <button class="btn clr dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    User Name
+     </button>
+     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+       <a class="dropdown-item" href="#">Setting</a>
+       <a class="dropdown-item" href="#">Logout</a>
+     </div>
+   </div>
+    </form>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+        </div>
+      </div>
+    </div>
+      <div class="sticky-top">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+          <div class="Pro-avatar">
+      <div class="ProAvatar">
+    <a class="ProAvatar-container" href="" data-resolved-url-large="" data-url="" target="_blank" rel="noopener" data-original-title="">
+        <img class="ProAvatar-image" src="../assets/profile.png" alt="">
+    </a>
+</div>
+    </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center" id="navbarContent">
+    <ul class="navbar-nav">
+      <li class="nav-item px-2">
+        <a class="nav-link" href="#">Batches</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="#">Staffs</a>
+      </li>  <li class="nav-item ">
+          <a class="nav-link" href="#">Students</a>
+        </li>
+        <li class="nav-item px-2">
+          <a class="nav-link" href="#">Events</a>
+        </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="#">Stories</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm-4" style="margin-top:150px;">
+<div class="card card1 hvr-grow-shadow">
+  <div class="card-body">
+    <div class="row">
+      <div style="margin-top:5px;">
+    <img src="../assets/university.png" width="80" height="80 " alt="university">
+</div>
+<div>
+<h4 class="text-center" style="color:teal;">Anna University</h4>
+    <div class="float-left px-2 text-center" style="font-size:20px;color:red;">
+      123<br>Staff
+    </div>
+    <div class="float-left px-2 text-center"  style="font-size:20px;color:blue;">
+    200<br>Student
+  </div>
+  <div class="float-left px-2 text-center"  style="font-size:20px;color:green;">
+  58<br>Parent
+</div>
+</div>
+</div>
+</div>
+<div>
+<a href="#" class="alarm-badge float-right px-3" data-badge="12" >
+<button class="btn button1">
+<i class="fa fa-comment"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="25" >
+<button class="btn button2">
+<i class="fa fa-heart"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="51" >
+<button class="btn button3">
+<i class="fa fa-newspaper-o"></i>
+</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-sm-4" style="margin-top:150px;">
+<div class="card card1 hvr-grow-shadow">
+<div class="card-body">
+<div class="row">
+  <div style="margin-top:5px;">
+<img src="../assets/university.png" width="80" height="80 " alt="university">
+</div>
+<div>
+<h4 class="text-center" style="color:teal;">Anna University</h4>
+<div class="float-left px-2 text-center" style="font-size:20px;color:red;">
+  123<br>Staff
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:blue;">
+200<br>Student
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:green;">
+58<br>Parent
+</div>
+</div>
+</div>
+</div>
+<div>
+<a href="#" class="alarm-badge float-right px-3" data-badge="12" >
+<button class="btn button1">
+<i class="fa fa-comment"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="25" >
+<button class="btn button2">
+<i class="fa fa-heart"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="51" >
+<button class="btn button3">
+<i class="fa fa-newspaper-o"></i>
+</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-sm-4" style="margin-top:150px;">
+<div class="card card1 hvr-grow-shadow">
+<div class="card-body">
+<div class="row">
+  <div style="margin-top:5px;">
+<img src="../assets/university.png" width="80" height="80 " alt="university">
+</div>
+<div>
+<h4 class="text-center" style="color:teal;">Anna University</h4>
+<div class="float-left px-2 text-center" style="font-size:20px;color:red;">
+  123<br>Staff
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:blue;">
+200<br>Student
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:green;">
+58<br>Parent
+</div>
+</div>
+</div>
+</div>
+<div>
+<a href="#" class="alarm-badge float-right px-3" data-badge="12" >
+<button class="btn button1">
+<i class="fa fa-comment"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="25" >
+<button class="btn button2">
+<i class="fa fa-heart"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="51" >
+<button class="btn button3">
+<i class="fa fa-newspaper-o"></i>
+</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-sm-4" style="margin-top:150px;">
+<div class="card card1 hvr-grow-shadow">
+<div class="card-body">
+<div class="row">
+  <div style="margin-top:5px;">
+<img src="../assets/university.png" width="80" height="80 " alt="university">
+</div>
+<div>
+<h4 class="text-center" style="color:teal;">Anna University</h4>
+<div class="float-left px-2 text-center" style="font-size:20px;color:red;">
+  123<br>Staff
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:blue;">
+200<br>Student
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:green;">
+58<br>Parent
+</div>
+</div>
+</div>
+</div>
+<div>
+<a href="#" class="alarm-badge float-right px-3" data-badge="12" >
+<button class="btn button1">
+<i class="fa fa-comment"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="25" >
+<button class="btn button2">
+<i class="fa fa-heart"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="51" >
+<button class="btn button3">
+<i class="fa fa-newspaper-o"></i>
+</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-sm-4" style="margin-top:150px;">
+<div class="card card1 hvr-grow-shadow">
+<div class="card-body">
+<div class="row">
+  <div style="margin-top:5px;">
+<img src="../assets/university.png" width="80" height="80 " alt="university">
+</div>
+<div>
+<h4 class="text-center" style="color:teal;">Anna University</h4>
+<div class="float-left px-2 text-center" style="font-size:20px;color:red;">
+  123<br>Staff
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:blue;">
+200<br>Student
+</div>
+<div class="float-left px-2 text-center"  style="font-size:20px;color:green;">
+58<br>Parent
+</div>
+</div>
+</div>
+</div>
+<div>
+<a href="#" class="alarm-badge float-right px-3" data-badge="12" >
+<button class="btn button1">
+<i class="fa fa-comment"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="25" >
+<button class="btn button2">
+<i class="fa fa-heart"></i>
+</button>
+</a>
+<a href="#" class="alarm-badge float-right px-3" data-badge="51" >
+<button class="btn button3">
+<i class="fa fa-newspaper-o"></i>
+</button>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.container-fluid {
+    height: 230px;
+    background-color:#b5a0ce;
+  }
+.clr{
+  background-color: #f8f9fa;
+}
+.inset img {
+    -webkit-background-size: 32px 32px;
+    background-size: 32px 32px;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+    display: block;
+    margin: -1px;
+    overflow: hidden;
+    position: relative;
+    height: 32px;
+    width: 32px;
+    z-index: 0;
+}
+.Pro-avatar {
+    bottom: -28px;
+    left: 200px;
+    position: absolute;
+    transition: bottom .3s;
+    z-index: 3;
+  }
+  .ProAvatar {
+    background: #fff;
+    border: 5px solid #fff;
+    border-radius: 50%;
+    height:150px;
+    position: relative;
+    width: 150px;
+}
+.ProAvatar-container {
+    outline: 0;
+}
+
+.u-block {
+    display: block!important;
+}
+
+.ProAvatar-image, .ProAvatar-placeholderImage {
+    -moz-force-broken-image-icon: 1;
+    border-radius: 50%;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+}
+img {
+    border: 0;
+}
+.hvr-grow-shadow {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: translateZ(0);
+  transform: translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+}
+.hvr-grow-shadow:hover, .hvr-grow-shadow:focus, .hvr-grow-shadow:active {
+  box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.5);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+.alarm-badge {
+  font-family: 'Helvetica Neue';
+  position: relative;
+  font-size:2em;
+  color: #333;
+  transition: all .2s ease-in-out;
+}
+.alarm-badge[data-badge]:after {
+  content: attr(data-badge);
+  position: absolute;
+  top: -5px;
+  left: 60%;
+  font-size: .4em;
+  font-weight: bold;
+  background:red;
+  color: #fff;
+  text-align: center;
+  border-radius:50%;
+  padding: 3px 5px;
+}
+.icon-float{
+  font-size:33px;
+	margin-top:45px;
+}
+.card1{
+  border-radius:5px;
+  box-shadow:1px 2px;
+  width:305px;
+  height:155px;
+}
+.btn-1{
+        border-radius:20px;
+        box-shadow: 2px 2px;
+        font-size: 18px;
+        color: #000;
+        margin: auto;
+}
+.button1{
+  position:224px 247px;
+  border-radius:60%;
+  background-color:#3DC0F9;
+  margin-top:-15px;
+}
+.button2 {
+  border-radius:60%;
+  background-color:#ff69b4;
+    margin-top:-15px;
+}
+.button3{
+  border-radius:60%;
+  background-color:#FFFF66 ;
+  margin-top:-15px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
+import Individual from '@/components/Individual'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/individual',
+      name: 'Individual',
+      component: Individual
     }
   ]
 })
